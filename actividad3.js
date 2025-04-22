2df1ea6955a77a9acc759cad4034122d6a61053a
adry24 <160780142+adry24@users.noreply.github.com>
2025-04-22 13:38:05 +0200

    Add files via upload

---
+++ actividad3.js
@@ -0,0 +1,6 @@
+let nombre = "Juan";
+let edad = "10";
+let altura = "1,92 metros";
+let casado = false;
+
+alert ("Mi nombre es: " + nombre + ", y tengo " + edad + " años, mido " + altura + " y mi estado de casado es: " + casado);
